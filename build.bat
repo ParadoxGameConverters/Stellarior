3f96e97a2f039265f87187fd68286b5adcac4ef4
Osariusz <osariusz@interia.pl>
2024-02-07 14:40:55 +0100

    Build fix

--- build.bat
+++ build.bat
@@ -4,6 +4,7 @@ copy Fronter\Release Release
 :: Requires a virtual environment with pyinstaller package in the pyinstaller folder
 pyinstaller\Scripts\pyinstaller.exe Stellarior\Source\main.py --onefile
 copy dist\main.exe Release\Stellarior
+xcopy Stellarior\Data_Files\blankMod\ Release\Stellarior\blankMod\ /E /Y
 
 python build.py
 pause
